5798942e8ca2f3e4c2809cc5395ff4942d94b3b2
kimdimka <kimdimka@yandex.ru>
2021-10-27 01:57:35 +1000

    1

---
+++ ExchangeMESManagerSevice/Services/ExchangeScenarios/BaseReferencesScenarios.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExchangeMESManagerSevice.Services.ExchangeScenarios
+{
+    public class BaseReferencesScenarios
+    {
+        private MESUoWService _MESUoWService;
+        private SQLUoWService _SQLUoWService;
+
+        public BaseReferencesScenarios(MESUoWService MESUoWService, SQLUoWService SQLUoWService)
+        {
+            _MESUoWService = MESUoWService;
+            _SQLUoWService = SQLUoWService;
+        }
+
+        private void GetEquipment()
+        {
+            var test = _SQLUoWService.EquipmentSpecificationSQLRepository.GetAll();
+
+
+        }
+
+        /// <summary>
+        /// Базовые справочники
+        /// </summary>
+        public void GetScenario1()
+        {
+            GetEquipment();
+        }
+
+    }
+}
